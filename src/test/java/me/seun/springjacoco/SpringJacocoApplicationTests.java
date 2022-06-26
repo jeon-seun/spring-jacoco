9a92dbd4d2ec548ca63902443290c1a6f372955a
seunjeon <jeonseun1234@gmail.com>
2022-06-26 21:48:07 +0900

    chore: jacoco 플러그인 설정
    - build.gradle 내 jacoco 설정 추가
    - test -> jacocoTestReports -> jacocoTestCoverageVerification 순 태스크 의존

--- src/test/java/me/seun/springjacoco/SpringJacocoApplicationTests.java
+++ src/test/java/me/seun/springjacoco/SpringJacocoApplicationTests.java
@@ -1,13 +1,23 @@
 package me.seun.springjacoco;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class SpringJacocoApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        Assertions.assertThat(applicationContext).isNotNull();
     }
 
 }
